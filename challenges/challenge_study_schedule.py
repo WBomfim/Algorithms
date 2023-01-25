fe165cc30af3a0d1945313ed158249a3a0abaaef
WBomfim <willianbomfim@hotmail.com>
2023-01-25 16:08:03 -0300

    feat: Criado a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    count = 0
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count += 1
+
+    except TypeError:
+        return None
+
+    return count
