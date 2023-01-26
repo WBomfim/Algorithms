0c418ce56e3a9ce6479a7a19f530d5e67078b8e3
WBomfim <willianbomfim@hotmail.com>
2023-01-26 17:11:19 -0300

    feat: Criado as funções is_anagram, merge_sort_string e merge_ordered - As funções são responsáveis por verificar se duas palavas distintas são um anagrama.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,41 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aquiii."""
-    raise NotImplementedError
+    first_string = merge_sort_string(first_string.lower())
+    second_string = merge_sort_string(second_string.lower())
+
+    if not first_string or not second_string:
+        return (first_string, second_string, False)
+
+    return (first_string, second_string, first_string == second_string)
+
+
+def merge_sort_string(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = merge_sort_string(string[:mid])
+    right = merge_sort_string(string[mid:])
+
+    return merge_ordered(left, right, string)
+
+
+def merge_ordered(left, right, string):
+    merged = list(string)
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+
+        if left[left_index] <= right[right_index]:
+            merged[left_index + right_index] = left[left_index]
+            left_index += 1
+        else:
+            merged[left_index + right_index] = right[right_index]
+            right_index += 1
+
+    for left_index in range(left_index, len(left)):
+        merged[left_index + right_index] = left[left_index]
+
+    for right_index in range(right_index, len(right)):
+        merged[left_index + right_index] = right[right_index]
+
+    return "".join(merged)
