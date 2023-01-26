1764d4a674073601559b75dde0f0a04a22eb54be
WBomfim <willianbomfim@hotmail.com>
2023-01-25 22:46:37 -0300

    test: Criado testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,34 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+
+KEY_INDEX_INVALID = [
+    ("verification", -2, "noitacifirev"), ("verification", 15, "noitacifirev"),
+]
+
+KEY_ODD_NUMBER = [
+    ("verification", 3, "rev_noitacifi"), ("verification", 5, "firev_noitaci"),
+]
+
+KEY_EVEN_NUMBER = [
+    ("verification", 4, "noitacif_irev"), ("verification", 6, "noitac_ifirev"),
+]
+
+KEY_INVALID_TYPE = [
+    (123, 4), ("verification", "4"), (None, 4), ("verification", None)
+]
 
 
 def test_encrypt_message():
-    pass
+    for message, key, encrypt in KEY_INDEX_INVALID:
+        assert encrypt_message(message, key) == encrypt
+
+    for message, key, encrypt in KEY_ODD_NUMBER:
+        assert encrypt_message(message, key) == encrypt
+
+    for message, key, encrypt in KEY_EVEN_NUMBER:
+        assert encrypt_message(message, key) == encrypt
+
+    for message, key in KEY_INVALID_TYPE:
+        with pytest.raises(TypeError):
+            encrypt_message(message, key)
