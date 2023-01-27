9104256fc6751166389d74245a4500607d2c94e4
WBomfim <willianbomfim@hotmail.com>
2023-01-27 19:18:48 -0300

    feat: Criado a função find_duplicate e count_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) == 1:
+        return False
+
+    for i in range(len(nums)):
+        if isinstance(nums[i], str) or nums[i] < 0:
+            return False
+
+        if count_duplicate(nums, nums[i]) > 1:
+            return nums[i]
+
+    return False
+
+
+def count_duplicate(nums, num):
+    count = 0
+
+    for i in range(len(nums)):
+        if nums[i] == num:
+            count += 1
+
+    return count
